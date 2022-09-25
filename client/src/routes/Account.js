ae99dabc2596662d32d565883f447e103323ad1c
arhumk <arhumk2430@gmail.com>
2022-09-25 06:23:15 -0700

    User Account

--- client/src/routes/Account.js
+++ client/src/routes/Account.js
@@ -1,7 +1,38 @@
+import React, { useEffect, useState} from 'react'
+
 const Account = () => {
+  const [data,setData]=useState([])
+
+  React.useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+    
+
+        const response = await fetch('http://localhost:9000/api/tickets/getConcerts')
+        const data = await response.json()
+        //console.log(this.state)
+        setData(data)
+        
+        //console.log(data)
+    }
+    fetchData()
+  
+    // call the function
+    //fetchData()
+      // make sure to catch any error
+      //.catch(console.error);
+  }, [])
+  console.log(data.title)
+
   return (
     <div>
-      <p>TEST</p>
+      <img src={data.imgSrc}/>
+     Info: {data.title}
+     <br/>
+     Description: {data.description}
+     <br/>
+     Price: {data.price}
+
     </div>
   );
 };
