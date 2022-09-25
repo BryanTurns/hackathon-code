3b402ea3ae80a0bfb69bafadc7713f62fcfbb38b
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2022-09-24 20:49:25 -0700

    Added getUser to backend

--- server/routes/users.js
+++ server/routes/users.js
@@ -18,8 +18,8 @@ router.get("/", (req, res) => {
 });
 
 // Getting one
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getUser, (req, res) => {
+  res.status(200).json(user.username);
 });
 
 // Create one
@@ -48,5 +48,19 @@ router.patch("/", (req, res) => {});
 
 // Delete one
 router.delete("/", (req, res) => {});
+
+async function getUser(req, res, next) {
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: "Cannot find user" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.user = user;
+  next();
+}
 
 module.exports = router;
